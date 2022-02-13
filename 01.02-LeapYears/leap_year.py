ea96eb21e0c953a84db57fb0989928ee597320a4
Eduardo Ubide <eubide@gmail.com>
2022-02-13 13:41:45 +0100

    add multiple of 4

---
+++ 01.02-LeapYears/leap_year.py
@@ -0,0 +1,9 @@
+# A leap year is defined as one that is divisible by 4,
+# but is not otherwise divisible by 100 unless it is also divisible by 400.
+
+
+def is_leap_year(number):
+    if not number % 4:
+        return True
+    else:
+        return False
